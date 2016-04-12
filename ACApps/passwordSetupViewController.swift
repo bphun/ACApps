3f5ee8b6b54a7168e8e9072c08663a37df2ab80c
Brandon <Brandon.phan.73@gmail.com>
2016-04-11 19:26:39 -0700

    Updated view hierarchy and improved sign in/up performance

--- ACApps/passwordSetupViewController.swift
+++ ACApps/passwordSetupViewController.swift
@@ -8,21 +8,76 @@
 
 import Foundation
 import UIKit
+import Firebase
 
-class passwordSetupViewController: UIViewController {
+class passwordSetupViewController: UIViewController, UITextFieldDelegate {
     
-    var firstName = String()
-    var lastName = String()
-    var email = String()
+    var firstName = ""
+    var lastName = ""
+    var email = ""
+    
+    @IBOutlet weak var usernameTextField: TextField!
+    @IBOutlet weak var passwordTextField: TextField!
+    @IBOutlet weak var confirmPasswordTextField: TextField!
+    @IBOutlet var textFieldCollection: [TextField]!
     @IBOutlet weak var doneBarButtonItem: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("First Name: \(firstName)",
-              "Last Name: \(lastName)",
-              "Email: \(email)")
+        
+        doneBarButtonItem.enabled = false
+        
+        usernameTextField.delegate = self
+        usernameTextField.returnKeyType = .Next
+        
+        passwordTextField.delegate = self
+        passwordTextField.returnKeyType = .Next
+        
+        confirmPasswordTextField.delegate = self
+        confirmPasswordTextField.returnKeyType = .Done
+
+        
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0)){ () -> Void in
+            //Setup NSNotificationCenter to notify us when all textFields are filled
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(passwordSetupViewController.nextTextField(_:)), name: UIKeyboardWillHideNotification, object: nil)
+            self.usernameTextField.addTarget(self, action: #selector(passwordSetupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+            self.passwordTextField.addTarget(self, action: #selector(passwordSetupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+            self.confirmPasswordTextField.addTarget(self, action: #selector(passwordSetupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+        }
     }
     
-    @IBAction func doneBarButtonItem(sender: AnyObject) {
+    @IBAction func doneButton(sender: UIBarButtonItem) {
+
     }
-}
+    
+    func nextTextField(sender: TextField) {
+        
+    }
+    
+    func textFieldDidChange(sender: TextField) {
+        for textField in textFieldCollection {
+            if textField.text?.utf16.count > 0 {
+                doneBarButtonItem.enabled = true
+            } else {
+                doneBarButtonItem.enabled = false
+            }
+        }
+    }
+    
+    func createAccount(firstName: String, lastName: String, email: String, username: String, psasword: String) {
+        let ref = Firebase(url: "https://acapps.firebaseio.com")
+        
+        ref.createUser(email, password: passwordTextField.text!) { (error, result) in
+            
+            if error != nil {
+                print("Error creating new user")
+            } else {
+                let UID = result["uid"] as? String
+                print("successfully created user with uid: \(UID)")
+            }
+            
+        }
+        
+    }
+    
+ }
--- ACApps/userSignupViewController.swift
+++ ACApps/userSignupViewController.swift
@@ -9,18 +9,22 @@
 import Foundation
 import UIKit
 import CryptoSwift
+import Firebase
 
 class userSignupViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var firstNameTextField: TextField!
     @IBOutlet weak var lastNameTextField: TextField!
     @IBOutlet weak var emailTextField: TextField!
+    @IBOutlet weak var passwordTextField: TextField!
+    @IBOutlet weak var confirmPasswordTextField: TextField!
     @IBOutlet var textFieldCollection: [TextField]!
     @IBOutlet weak var nextBarButtonItem: UIBarButtonItem!
     
-    var firstName = String()
-    var lastName = String()
-    var email = String()
+    var userFirstName = String()
+    var userLastName = String()
+    var userEmail = String()
+    var userPassword = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,13 +40,23 @@ class userSignupViewController: UIViewController, UITextFieldDelegate {
         lastNameTextField.returnKeyType = .Next
         
         emailTextField.delegate = self
-        emailTextField.returnKeyType = .Continue
+        emailTextField.returnKeyType = .Next
         
-        // Setup NSNotificationCenter to notify us when all textFields are filled 
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(userSignupViewController.nextTextField(_:)), name: UIKeyboardWillHideNotification, object: nil)
-        firstNameTextField.addTarget(self, action: #selector(userSignupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
-        lastNameTextField.addTarget(self, action: #selector(userSignupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
-        emailTextField.addTarget(self, action: #selector(userSignupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+        passwordTextField.delegate = self
+        passwordTextField.returnKeyType = .Next
+        
+        confirmPasswordTextField.delegate = self
+        confirmPasswordTextField.returnKeyType = .Continue
+        
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0)) { () -> Void in
+            // Setup NSNotificationCenter to notify us when all textFields are filled
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(userSignupViewController.nextTextField(_:)), name: UIKeyboardWillHideNotification, object: nil)
+            self.firstNameTextField.addTarget(self, action: #selector(userSignupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+            self.lastNameTextField.addTarget(self, action: #selector(userSignupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+            self.emailTextField.addTarget(self, action: #selector(userSignupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+            self.passwordTextField.addTarget(self, action: #selector(userSignupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+            self.confirmPasswordTextField.addTarget(self, action: #selector(userSignupViewController.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+        }
     }
     
     func nextTextField(sender: TextField) {
@@ -50,35 +64,109 @@ class userSignupViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidChange(sender: TextField) {
-
+        
         for textField in textFieldCollection {
-            if textField.text?.utf16.count > 0 {
-                nextBarButtonItem.enabled = true
+            if textField.text?.utf16.count > 0 && isvalidEmailCheck(emailTextField.text!){
+                UIView.animateWithDuration(10.0, animations: {
+                    self.nextBarButtonItem.enabled = true
+                })
             } else {
                 nextBarButtonItem.enabled = false
             }
         }
+        
     }
 
     @IBAction func nextBarButtonItem(sender: UIBarButtonItem) {
-        if nextBarButtonItem.enabled == true {
-            dispatch_async(dispatch_get_main_queue()) {
-                self.presentView("passwordSetupView", animated: true)
-                
+        createUserAndPresentNextView()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if getCurrentTextField() == firstNameTextField {
+            firstNameTextField.resignFirstResponder()
+            lastNameTextField.becomeFirstResponder()
+        } else if getCurrentTextField() == lastNameTextField {
+            lastNameTextField.resignFirstResponder()
+            emailTextField.becomeFirstResponder()
+        } else if getCurrentTextField() == emailTextField {
+            emailTextField.resignFirstResponder()
+            createUserAndPresentNextView()
+        } else if getCurrentTextField() == passwordTextField {
+            for textField in textFieldCollection {
+                if textField.text == "" {
+                    textField.becomeFirstResponder()
+                }
             }
+            passwordTextField.resignFirstResponder()
+            confirmPasswordTextField.becomeFirstResponder()
+        }
+        return true
+    }
+    
+    func getCurrentTextField() -> TextField {
+        
+        var currentActiveTextField: TextField!
+        
+        for textField in textFieldCollection {
+            if textField.isFirstResponder() {
+                currentActiveTextField = textField
+            }
+        }
+        return currentActiveTextField
+    }
+    
+    func createUserAndPresentNextView() {
+        if firstNameTextField.text! != "" && lastNameTextField.text! != "" && emailTextField.text! != "" && passwordTextField.text! != "" && confirmPasswordTextField.text! != "" {
+            // All textFields are filled and barButtonItem is enabled
+            self.userFirstName = firstNameTextField.text!
+            self.userLastName = lastNameTextField.text!
+            self.userEmail = emailTextField.text!
+            self.userPassword = passwordTextField.text!
+            
+            // Resign all textFields and create the user
+            dispatch_async(dispatch_get_main_queue()) {
+                for textField in self.textFieldCollection {
+                    if textField.isFirstResponder() {
+                        textField.resignFirstResponder()
+                    }
+                }
+                
+                self.createUser(self.userEmail, password: self.userPassword, firstName: self.userFirstName, lastName: self.userLastName, completionHandler: { (success) -> Void in
+                    if (success) {
+                        // Success creating the user, present the next view
+                        dispatch_async(dispatch_get_main_queue()) {
+                            self.presentView("mapViewViewController", animated: true)
+                            print("Show next view")
+                        }
+                    } else if self.accountCreationError == true {
+                    
+                        // Error creating the user
+                        let accountCreationErrorAlertView = SCLAlertView()
+                        accountCreationErrorAlertView.addButton("Ok", action: {
+                            accountCreationErrorAlertView.removeFromParentViewController()
+                        })
+                        accountCreationErrorAlertView.showError("Error", subTitle: "Error creating account")
+                    }
+                })
+            }
+
+        } else {
+            // Not all textFields are enabled
+        
         }
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let destinationViewController: passwordSetupViewController = segue.destinationViewController as! passwordSetupViewController
-        
-        firstName = firstNameTextField.text!
-        lastName = lastNameTextField.text!
-        email = emailTextField.text!
-        
-        destinationViewController.firstName = firstName
-        destinationViewController.lastName = lastName
-        destinationViewController.email = email
+        let segueId = "passwordSetupSegue"
+        if segue.identifier == segueId {
+            print("Prepare for segue: \(segueId)")
+            
+            if let destinationVC: passwordSetupViewController = segue.destinationViewController as? passwordSetupViewController {
+                destinationVC.firstName = userFirstName
+                destinationVC.lastName = userLastName
+                destinationVC.email = userEmail
+            }
+        }
     }
     
     func presentView(VCID: String, animated: Bool) {
@@ -86,6 +174,34 @@ class userSignupViewController: UIViewController, UITextFieldDelegate {
         self.navigationController?.pushViewController(VC, animated: animated)
     }
     
+    func isvalidEmailCheck(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluateWithObject(email)
+        
+    }
     
-    
+    typealias CompletionHandler = (success: Bool) -> Void
+    var createdUser: Bool!
+    var accountCreationError: Bool!
+    func createUser(email: String, password: String, firstName: String, lastName: String, completionHandler: CompletionHandler) {
+        
+        let ref = Firebase(url: "https://acapps.firebaseio.com")
+        
+        ref.createUser(email, password: password) { (error, result) in
+            
+            if error != nil {
+                print("Error: \(error)")
+                self.createdUser = false
+                self.accountCreationError = true
+            } else {
+                print(String(result))
+                self.createdUser = true
+                self.accountCreationError = false
+                completionHandler(success: self.createdUser)
+            }
+            
+        }
+        
+    }
 }
